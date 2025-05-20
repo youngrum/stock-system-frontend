3ff985457dfa3e84f81945652ea9f901df0a0da9
youngrum <vinoforjob@gmail.com>
2025-05-20 21:49:55 +0900

    Linter調整 分岐加

--- src/components/inventory/InventoryTable.tsx
+++ src/components/inventory/InventoryTable.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import { InventoryItem } from '@/types/InventoryItem'
-import { ExternalLink, PackagePlus, Truck } from 'lucide-react';
-import { formatDate } from '@/lib/utils/dateFormat';
+import Link from "next/link";
+import { InventoryItem } from "@/types/InventoryItem";
+import { ExternalLink, PackagePlus, Truck } from "lucide-react";
+import { formatDate } from "@/lib/utils/dateFormat";
 
 export default function InventoryTable({
   data,
   onReceive,
-  onDispach
+  onDispach,
 }: {
   data: InventoryItem[];
   onReceive: (item: InventoryItem) => void;
@@ -15,7 +15,7 @@ export default function InventoryTable({
   return (
     <div className="overflow-x-auto bg-white">
       <table className="w-full text-sm text-center border-collapse">
-        <thead style={{ color: '#101540' }} >
+        <thead style={{ color: "#101540" }}>
           <tr className="font-semibold">
             <th className="text-base px-4 py-3">入庫</th>
             <th className="text-base px-4 py-3">出庫</th>
@@ -28,28 +28,49 @@ export default function InventoryTable({
             <th className="text-base px-4 py-3">履歴</th>
           </tr>
         </thead>
-        <tbody className="text-gray-700" style={{ borderBottom: '1px solid #101540' }}>
+        <tbody
+          className="text-gray-700"
+          style={{ borderBottom: "1px solid #101540" }}
+        >
           {data.map((item) => (
-            <tr key={item.itemCode} className="hover:bg-gray-200" >
+            <tr key={item.itemCode} className="hover:bg-gray-200">
               <td className="px-4 py-3 items-center">
-                <Link href="#" onClick={() => onReceive(item)} className="text-[#0d113d]"><PackagePlus  className="mx-auto"/></Link>
+                <Link
+                  href="#"
+                  onClick={() => onReceive(item)}
+                  className="text-[#0d113d]"
+                >
+                  <PackagePlus className="mx-auto" />
+                </Link>
               </td>
               <td className="px-4 py-3">
-                <Link href="#" onClick={() => onDispach(item)} className="text-[#0d113d]"><Truck  className="mx-auto"/></Link>
+                <Link
+                  href="#"
+                  onClick={() => onDispach(item)}
+                  className="text-[#0d113d]"
+                >
+                  <Truck className="mx-auto" />
+                </Link>
               </td>
               <td className="px-4 py-3">{item.itemCode}</td>
-              <td className="px-4 py-3">{item.itemName}</td>
+              <td className="px-4 py-3 max-w-[300px] truncate">
+                {item.itemName}
+              </td>
               <td className="px-4 py-3">{item.category}</td>
-              <td className="px-4 py-3">{item.modelNumber ?? '-'}</td>
+              <td className="px-4 py-3 max-w-[300px] truncate">
+                {item.modelNumber ?? "-"}
+              </td>
               <td className="px-4 py-3">{item.currentStock}</td>
               <td className="px-4 py-3">{formatDate(item.lastUpdated)}</td>
               <td className="px-4 py-3">
-                <Link href="#" className="text-[#0d113d]"><ExternalLink className="mx-auto"/></Link>
+                <Link href="#" className="text-[#0d113d]">
+                  <ExternalLink className="mx-auto" />
+                </Link>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }

1bdf452126bde9ebec1fadfa8f9a191691ec7af3
youngrum <vinoforjob@gmail.com>
2025-06-13 08:52:27 +0900

    standaloneビルド用設定 型定義修正

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
